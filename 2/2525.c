a1a93a35ed32f2886885b140affa79344a0bab2c
rainbowpopcorn1 <junobaek2009@gmail.com>
2023-06-23 19:45:11 +0900

    백준 2525

--- 2/2525.c
+++ 2/2525.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    int a, b, c, x, y;
-    scanf("%d %d\n%d", &a, &b, &c);
-    x = (a + (b + c) / 60) % 24;
-    y = (b + c) % 60;
-    printf("%d %d", x, y);
+	int a, b, c, x, y;
+	scanf("%d %d\n%d", &a, &b, &c);
+	x = (a + (b + c) / 60) % 24;
+	y = (b + c) % 60;
+	printf("%d %d", x, y);
 }
